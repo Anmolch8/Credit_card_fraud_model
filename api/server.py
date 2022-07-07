810dc86c674eb68afd464720c99120c615ed5fbe
Anmol chawla <chawllaa.anmol@gmail.com>
2022-07-07 16:47:43 +0530

    server changes

---
+++ api/server.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flask import Flask,request,jsonify
+import joblib
+
+app=Flask(__name__)
+model=joblib.load('ML_PIPELINE/model/fraud-detector.pkl')
+@app.route('/')
+def test():
+    return 'heyeye'
+@app.route('/pre')
+def predict():
+    distance_from_home= float(request.args["home_distance"])
+    distance_from_last_transaction=float(request.args["last_transaction"])
+    repeat_retailer=int(request.args["retailer"])
+    used_card=int(request.args["card"])
+    used_pin=int(request.args["pin"])
+    online_order=int(request.args["order"])
+    perdiction=model.predict([[distance_from_home,distance_from_last_transaction,repeat_retailer,
+    used_card,used_pin,online_order]])
+    out=perdiction[0]
+    return {'prediction':int(out)}
+if __name__ == '__main__':
+    app.run(debug=True)    
